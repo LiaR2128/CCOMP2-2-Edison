dc79c858bb7274ea3a2c7e979b5cad88f3558772
Niclaii <nicolas.quintanilla@ucsp.edu.pe>
2022-10-06 08:27:14 -0500

    otro cry cry moment

--- Semana 5/ordenamiento/burbuja.cpp
+++ Semana 5/ordenamiento/burbuja.cpp
@@ -4,6 +4,8 @@
 
 #include <iostream>
 
+#include "point.h"
+
 
 using namespace std;
 
@@ -62,6 +64,49 @@ using namespace std;
 } 
 
 
+    void bsortpuntos(point arr [], int elements){
+         for (int i =0 ; i< elements; i ++) {
+
+
+        for (int j = i+1; j<elements-1; j++){
+
+            if (arr[i].getX()> arr [j].getX()){
+
+                //se deben intercambiar puntos
+                /*int temp = arr[j];
+
+                
+
+                arr[j]= arr[j+1];
+                arr[j+1]=temp;
+                */
+
+            }
+        }
+    }
+
+ }
+        
+    void insertionspoints(point arr[], int n) { 
+
+    point key;
+    int j; 
+    for (int i = 1; i < n; i++)
+    { 
+        key = arr[i]; 
+        j = i - 1; 
+
+        while (j >= 0 && arr[j].getX() > key.getX())
+        { 
+            arr[j + 1] = arr[j]; 
+            j = j - 1; 
+        } 
+        arr[j + 1] = key; 
+    } 
+} 
+
+
+
 
 
 
@@ -89,10 +134,14 @@ int main () {
 
     imprimirarreglo(arreglo, 4);
 
+    //sizeof -- retorna el tamaño en bytes del tipo de dato
+
+    int tam = sizeof(arreglo) / sizeof(arreglo[0]);
 
 
-
-
+    imprimirarreglo(arreglo, tam);
+    insertionSort(arreglo, tam);
+    imprimirarreglo(arreglo, tam);
 
 
     return 0;
